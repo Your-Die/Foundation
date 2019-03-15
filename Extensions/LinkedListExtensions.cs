c871e010d574038431f8871e77c3828eadf63dd6
Jordy <jordy_van_opstal@live.nl>
2019-03-15 21:10:32 +0100

    Add LinkedListExtensions.

---
+++ Extensions/LinkedListExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class LinkedListExtensions
+{
+    /// <summary>
+    /// Finds the <see cref="LinkedListNode{T}"/> in the <paramref name="list"/> where the value satisfies the <paramref name="predicate"/>.
+    /// </summary> 
+    public static LinkedListNode<T> FindNode<T>(this LinkedList<T> list, Func<T, bool> predicate)
+    {
+        for (LinkedListNode<T> node = list.First; node != null; node = node.Next)
+            if (predicate(node.Value))
+                return node;
+
+        return null;
+    }
+
+    /// <summary>
+    /// Adds the <paramref name="value"/> to the <paramref name="list"/> after the first value that satisfies the <paramref name="predicate"/>.
+    /// </summary> 
+    public static void AddAfter<T>(this LinkedList<T> list, T value, Func<T, bool> predicate)
+    {
+        LinkedListNode<T> node = list.FindNode(predicate) ?? list.Last;
+        list.AddAfter(node, value);
+    }
+
+    /// <summary>
+    /// Adds the <paramref name="value"/> to the <paramref name="list"/> before the first value that satisfies the <paramref name="predicate"/>.
+    /// </summary> 
+    public static void AddBefore<T>(this LinkedList<T> list, T value, Func<T, bool> predicate)
+    {
+        LinkedListNode<T> node = list.FindNode(predicate) ?? list.Last;
+        list.AddBefore(node, value);
+    }
+}
