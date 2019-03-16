b363273cbed56182f3d7012c159e498cc00cbf9d
Jordy <jordy_van_opstal@live.nl>
2019-03-16 13:55:35 +0100

    Add UnityHelper.

---
+++ UnityHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Chinchillada.Utilities
+{
+    public static class UnityHelper
+    {
+        public static T FindOrCreate<T>() where T : Component
+        {
+            T instance = Object.FindObjectOfType<T>();
+            return instance != null ? instance : CreateObject<T>();
+        }
+
+        public static T CreateObject<T>() where T : Component
+        {
+            GameObject gameObject = new GameObject(nameof(T));
+            return gameObject.AddComponent<T>();
+        }
+    }
+}
