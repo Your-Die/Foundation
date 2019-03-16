853ddd6d83cbb1a710c6f12069be61bff9ecadf4
Jordy <jordy_van_opstal@live.nl>
2019-03-16 15:27:16 +0100

    Add Random.

---
+++ Helpers/Random.cs
@@ -0,0 +1,30 @@
+﻿using RNG = UnityEngine.Random;
+
+namespace Chinchillada.Utilities
+{
+    public static class Random
+    {
+        public static float Value => RNG.value;
+        public static float value => RNG.value;
+
+        public static int Range(int max)
+        {
+            return Range(0, max);
+        }
+
+        public static int Range(int min, int max)
+        {
+            return RNG.Range(min, max);
+        }
+
+        public static float Range(float max)
+        {
+            return Range(0, max);
+        }
+
+        public static float Range(float min, float max)
+        {
+            return RNG.Range(min, max);
+        }
+    }
+}
