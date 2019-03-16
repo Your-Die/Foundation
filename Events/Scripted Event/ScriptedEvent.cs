b3160acab0eb300b89df7f8d7100452652b53945
Jordy <jordy_van_opstal@live.nl>
2019-03-16 11:05:20 +0100

    Add Scripted Events.

---
+++ Events/Scripted Event/ScriptedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Chinchillada/Scripted Event", fileName = "Event")]
+public class ScriptedEvent : ScriptableObject
+{ 
+    public event Action Raised;
+
+    public void Raise()
+    { 
+        Raised?.Invoke();
+    } 
+}
---
+++ Events/Scripted Event/ScriptedEventListener.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ScriptedEventListener : MonoBehaviour
+{
+    [SerializeField] private ScriptedEvent _event;
+
+    public UnityEvent Response;
+
+    private void OnEnable()
+    {
+        _event.Raised += OnEventRaised;
+    }
+
+    private void OnDisable()
+    {
+        _event.Raised -= OnEventRaised;
+    }
+
+    public void OnEventRaised()
+    {
+        Response?.Invoke();
+    }
+}
