7f22daa9f796d89620c0eadce2fab3531dd31db0
Jordy <jordy_van_opstal@live.nl>
2019-03-16 14:06:07 +0100

    Add singleton implementation.

---
+++ SingletonBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Chinchillada.Utilities
+{
+    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private T _instance;
+
+        public T Instance
+        {
+            get
+            {
+                if (_instance != null)
+                    return _instance;
+
+                _instance = UnityHelper.FindOrCreate<T>();
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+                return;
+
+            Debug.Log($"Duplicate singleton of type ({nameof(T)}) awoken. Destroying {this.name}");
+            Destroy(gameObject);
+        }
+    }
+}
