172737b521f6931796b4466f6d926f5583d7eede
Jordy <jordy_van_opstal@live.nl>
2019-03-16 11:03:08 +0100

    Add keyboard events.

---
+++ Events/Keyboard/KeyboardEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Chinchillada.Utilities
+{
+    [Serializable]
+    public class KeyboardEvent
+    {
+        [SerializeField] private KeyCode _key;
+
+        [SerializeField] private UnityEvent _keyDown;
+        [SerializeField] private UnityEvent _keyUp;
+
+        public UnityEvent KeyDown => _keyDown;
+        public UnityEvent KeyUp => _keyUp;
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(_key))
+                KeyDown.Invoke();
+
+            else if (Input.GetKeyUp(_key))
+                KeyUp.Invoke();
+        }
+    }
+}
---
+++ Events/Keyboard/KeyboardEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chinchillada.Utilities
+{
+    public class KeyboardEvents : MonoBehaviour
+    {
+        [SerializeField] private List<KeyboardEvent> _keyboardEvents = new List<KeyboardEvent>();
+
+        private void Update()
+        {
+            foreach (var keyboardEvent in _keyboardEvents)
+                keyboardEvent.Update();
+        }
+    }
+}
