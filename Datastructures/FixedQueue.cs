cb49649865405442a799a264f4922f98e8428771
Jordy <jordy_van_opstal@live.nl>
2019-03-16 15:28:06 +0100

    Add Datastructures.

---
+++ Datastructures/FixedQueue.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chinchillada.Utilities
+{
+    /// <summary>
+    /// Wrapper class for Queue which stays at a fixed capacity, and automatically dequeues values when the capacity is exceeded.
+    /// </summary>
+    /// <typeparam name="T">The type of the values in the queue.</typeparam>
+    public class FixedQueue<T>
+    {
+        /// <summary>
+        /// The queue instance that this class wraps.
+        /// </summary>
+        private readonly Queue<T> _queue;
+        /// <summary>
+        /// The fixed size for this queue.
+        /// </summary>
+        private int _capacity;
+       
+        /// <summary>
+        /// Event that is called when an excess element is dequeued.
+        /// </summary>
+        public event Action<T> ExcessDequeued;
+
+        /// <summary>
+        /// Event this is called when a element is enqueued.
+        /// </summary>
+        public event Action<T> ValueEnqueued;
+
+        /// <summary>
+        /// The capacity that the queue may not exceed.
+        /// </summary>
+        public int Capacity
+        {
+            get { return _capacity; }
+            set
+            {
+                //Do nothing if it's the same.
+                if (value == _capacity)
+                    return;
+
+                //Look if elements need to be dequeued.
+                _capacity = value; 
+                DequeueExcessValues();
+            }
+        }
+
+        /// <summary>
+        /// Amount of values in this queue.
+        /// </summary>
+        public int Count => _queue.Count;
+
+        public FixedQueue(int capacity)
+        {
+            _capacity = capacity;
+            _queue = new Queue<T>(capacity + 1);
+        }
+
+        /// <summary>
+        /// Enqueues the <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The value to enqueue.</param>
+        public void Enqueue(T value)
+        {
+            _queue.Enqueue(value);
+            ValueEnqueued?.Invoke(value);
+
+            DequeueExcessValues();
+        }
+
+        /// <summary>
+        /// Returns the element at the <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns>The value at <paramref name="index"/>.</returns>
+        public T AtIndex(int index)
+        {
+            return _queue.ElementAt(index);
+        }
+
+        /// <summary>
+        /// Converts the queue to a list.
+        /// </summary>
+        /// <returns></returns>
+        public List<T> ToList()
+        {
+            return _queue.ToList();
+        }
+
+        /// <summary>
+        /// Clears the queue.
+        /// </summary>
+        public void Clear()
+        {
+            _queue.Clear();
+        }
+
+        /// <summary>
+        /// Dequeues until the queue doesn't exceed capacity anymore.
+        /// </summary>
+        private void DequeueExcessValues()
+        {
+            while (_queue.Count > Capacity)
+            {
+                T value = _queue.Dequeue();
+                ExcessDequeued?.Invoke(value);
+            }
+        }
+    }
+}
---
+++ Datastructures/Pair.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Chinchillada.Utilities
+{
+    /// <summary>
+    ///  Simple struct for a pair of values.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal struct Pair<T>
+    {
+        /// <summary>
+        /// The first value in this pair.
+        /// </summary>
+        public T Item1 { get; set; }
+
+        /// <summary>
+        /// The second value in this pair.
+        /// </summary>
+        public T Item2 { get; set; }
+
+        /// <summary>
+        /// Constructs a new pair.
+        /// </summary>
+        /// <param name="item1"><see cref="Item1"/></param>
+        /// <param name="item2"><see cref="Item2"/></param>
+        public Pair(T item1, T item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        /// <summary>
+        /// Applies a predicate to this pair's items.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>the result of the predicate.</returns>
+        public bool ApplyPredicate(Func<T, T, bool> predicate)
+        {
+            return predicate(Item1, Item2);
+        }
+    }
+}
