3509ae71545ae5b406e9251a3ce67f4230453aef
Jordy_DeSync <jordy_van_opstal@live.nl>
2019-03-22 18:21:13 +0100

    Add enumerables helper.

---
+++ Helpers/Enumerables.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Chinchillada.Utilities
+{
+    public static class Enumerables
+    {
+        public static IEnumerable<T> Single<T>(T element)
+        {
+            yield return element;
+        }
+
+        public static IEnumerable<T> Repeat<T>(T element)
+        {
+            while (true)
+                yield return element;
+        }
+    }
+}
