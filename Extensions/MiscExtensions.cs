451c3841d794088ccafbbf6b0b939ef0e72d371e
Jordy <jordy_van_opstal@live.nl>
2019-04-20 14:07:10 +0200

    Implement ToBinary for booleans.

---
+++ Extensions/MiscExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Chinchillada.Utilities
+{
+    public static class MiscExtensions
+    {
+        public static int ToBinary(this bool value)
+        {
+            return value ? 1 : 0;
+        }
+    }
+}
