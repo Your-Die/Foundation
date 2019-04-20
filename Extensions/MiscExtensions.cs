fe5b203ee267b74f7081820bc6bde1cfbf8ba9d9
Jordy <jordy_van_opstal@live.nl>
2019-04-20 16:56:50 +0200

    Implement Until.
    Repeats a generator function until it generates something that satisfies the predicate.

--- Extensions/MiscExtensions.cs
+++ Extensions/MiscExtensions.cs
@@ -1,10 +1,24 @@
-﻿namespace Chinchillada.Utilities
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Chinchillada.Utilities
 {
     public static class MiscExtensions
     {
         public static int ToBinary(this bool value)
         {
             return value ? 1 : 0;
+        }
+
+        public static T Until<T>(this Func<T> generator, Func<T, bool> predicate)
+        {
+            T generation;
+            do
+            {
+                generation = generator();
+            } while (!predicate(generation));
+
+            return generation;
         }
     }
 }
