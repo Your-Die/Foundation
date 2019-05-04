cc7fc9d6d46601c4c0854cd550af11338304ade6
Jordy <jordy_van_opstal@live.nl>
2019-05-04 16:24:50 +0200

    Implement Markov Distribution.

---
+++ Distributions/Markov/Markov.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+
+namespace Chinchillada.Distributions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class Markov<T> : IDistribution<IEnumerable<T>>
+    {
+        private readonly IDistribution<T> _initial;
+
+        private readonly Func<T, IDistribution<T>> _transition;
+
+        public static Markov<T> Distribution(IDistribution<T> initial, Func<T, IDistribution<T>> transition)
+        {
+            return new Markov<T>(initial, transition);
+        }
+
+        private Markov(IDistribution<T> initial, Func<T, IDistribution<T>> transition)
+        {
+            _initial = initial;
+            _transition = transition;
+        }
+
+        public IEnumerable<T> Sample()
+        {
+            var current = _initial;
+            while (true)
+            {
+                if (current is Empty<T>)
+                    break;
+
+                T sample = current.Sample();
+                yield return sample;
+
+                current = _transition(sample);
+            }
+        }
+    }
+}
