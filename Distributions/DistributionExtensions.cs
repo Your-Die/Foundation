b6533d3eee0666a697adc43552c3c1a684aabe73
Jordy <jordy_van_opstal@live.nl>
2019-05-21 17:05:18 +0200

    Rename Distribution.cs to DistributionExtensions.

--- Distributions/DistributionExtensions.cs
+++ Distributions/DistributionExtensions.cs
@@ -1,11 +1,14 @@
-﻿namespace Chinchillada.Distributions
+﻿using System;
+using System.Linq;
+
+namespace Chinchillada.Distributions
 {
     using System.Collections.Generic;
 
     /// <summary>
     /// Extension method container for <see cref="IDistribution{T}"/>.
     /// </summary>
-    public static class Distribution
+    public static class DistributionExtensions
     {
         /// <summary>
         /// Infinite enumerable of samples from this <see cref="IDistribution{T}"/>.
@@ -20,5 +23,11 @@
                 yield return distribution.Sample();
             }
         }
+
+        public static float ExpectedValue<T>(this IDistribution<T>distribution, Func<T,float> function, int sampleSize = 1000) 
+            => distribution.Samples().Take(sampleSize).Select(function).Average();
+
+        public static float ExpectedValue(this IDistribution<float> distribution, int sampleSize = 1000)
+            => distribution.ExpectedValue(x => x, sampleSize);
     }
 }
