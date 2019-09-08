e822507fdf1d912737b36516ddafd17ad8294827
Jordy <jordy_van_opstal@live.nl>
2019-09-08 18:58:07 +0200

    Create sum and average for collections of vectors.

---
+++ Extensions/VectorExtensions.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Chinchillada.Utilities
+{
+    public static class VectorExtensions
+    {
+        public static Vector3 Average(this IEnumerable<Vector3> vectors)
+        {
+            var list = vectors.ToList();
+            var sum = list.Sum();
+
+            return sum / list.Count;
+        }
+
+        public static Vector3 Sum(this IEnumerable<Vector3> vectors)
+        {
+            return vectors.Aggregate(Vector3.zero, (current, vector) => current + vector);
+        }
+    }
+}
