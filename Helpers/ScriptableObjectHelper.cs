2a5af601f2440b369e91d01b7459e707b6a13916
Jordy <jordy_van_opstal@live.nl>
2019-11-03 15:36:42 +0100

    Implement ScriptableObject helper.

---
+++ Helpers/ScriptableObjectHelper.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Chinchillada.Utilities
+{
+    public static class ScriptableObjectHelper
+    {
+        /// <summary>
+        //  This makes it easy to create, name and place unique new ScriptableObject asset files.
+        /// </summary>
+        public static T CreateAsset<T>() where T : ScriptableObject
+        {
+            var asset = ScriptableObject.CreateInstance<T>();
+
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            if (path == "")
+            {
+                path = "Assets";
+            }
+            else if (Path.GetExtension(path) != "")
+            {
+                var fileName = Path.GetFileName(path);
+                path = path.Replace(fileName, "");
+            }
+
+            var assetPathAndName = AssetDatabase.GenerateUniqueAssetPath($"{path}/New {typeof(T)}.asset");
+
+            AssetDatabase.CreateAsset(asset, assetPathAndName);
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+
+            return asset;
+        }
+    }
+}
