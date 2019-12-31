5d6298e45434f48bc285e35fe810b09bdda5b672
Jordy <jordy_van_opstal@live.nl>
2019-12-31 18:12:01 +0100

    Add misc extensions.

--- Extensions/MiscExtensions.cs
+++ Extensions/MiscExtensions.cs
@@ -22,5 +22,18 @@ namespace Chinchillada.Utilities
         }
 
         public static int AsBinary(this bool value) => value ? 1 : 0;
+
+        public static bool IsUneven(this int value)
+        {
+            return value % 2 != 0;
+        }
+        
+        public static (int width, int height) GetShape<T>(this T[,] array)
+        {
+            var width = array.GetLength(0);
+            var height = array.GetLength(1);
+
+            return (width, height);
+        }
     }
 }
