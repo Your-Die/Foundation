2a68bd65f07f1a41c889c10c4a58d822022206b5
Jordy <jordy_van_opstal@live.nl>
2019-12-31 13:29:28 +0100

    Implement simple camera mover.
    Moves with default axis.

---
+++ Behaviours/CameraMover.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Chinchillada.Utilities
+{
+    public class CameraMover : ChinchilladaBehaviour
+    {
+        [SerializeField] private float moveSpeed = 1;
+        [SerializeField] private float zoomSpeed = 1;
+        
+        [SerializeField] private string horizontalAxis = "Horizontal";
+        [SerializeField] private string verticalAxis = "Vertical";
+
+        [SerializeField] private string zoomAxis = "Mouse Scroll";
+        
+        [SerializeField, FindComponent] private Camera cam;
+
+        private void Update()
+        {
+            // Read input.
+            var horizontalInput = Input.GetAxis(this.horizontalAxis);
+            var verticalInput = Input.GetAxis(this.verticalAxis);
+            var zoomInput = Input.GetAxis(this.zoomAxis);
+
+            // Cache time.
+            var deltaTime = Time.deltaTime;
+            
+            // Calculate vectors.
+            var movement = this.moveSpeed * deltaTime * new Vector3(horizontalInput, verticalInput);
+            var zoom = this.zoomSpeed * deltaTime * zoomInput * Vector3.back;
+
+            // Combine and apply.
+            var translation = movement + zoom;
+            this.cam.transform.Translate(translation);
+        }
+    }
+}
