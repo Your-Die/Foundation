a4aa5e094c85fbcab26b738c40337670680b8612
Jordy <jordy_van_opstal@live.nl>
2020-01-01 14:22:00 +0100

    Implement SingleInstanceBehaviour.

---
+++ Behaviours/SingleInstanceBehaviour.cs
@@ -0,0 +1,25 @@
+using Chinchillada;
+using UnityEngine;
+
+namespace Utilities.Behaviours
+{
+    public abstract class SingleInstanceBehaviour<T> : ChinchilladaBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        public static bool HasInstance => Instance != null;
+
+        protected abstract T GetInstance();
+
+        protected void Awake()
+        {
+            if (HasInstance)
+            {
+                Debug.Log($"Duplicate single-object of type ({nameof(T)}) awoken. Destroying {this.name}");
+                Destroy(this.gameObject);
+            }
+            else
+                Instance = this.GetInstance();
+        }
+    }
+}
