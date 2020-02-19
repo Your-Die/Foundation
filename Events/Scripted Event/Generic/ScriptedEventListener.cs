0a981565dbbfe1f3287ba0d979bd5c9d8f4d7d8a
Crowcontrol <jordy_van_opstal@live.nl>
2020-02-19 15:37:25 +0100

    Add generic ScriptedEventListener

---
+++ Events/Scripted Event/Generic/ScriptedEventListener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Chinchillada.Events
+{
+    public abstract class ScriptedEventListener<T> : ChinchilladaBehaviour
+    {
+        /// <summary>
+        /// The <see cref="ScriptedEvent"/> we listen to.
+        /// </summary>
+        [SerializeField] private ScriptedEvent<T> @event = null;
+
+        /// <summary>
+        /// The event invoked when the <see cref="Event"/> is raised.
+        /// </summary>
+        public UnityEvent Response;
+
+        /// <summary>
+        /// The <see cref="ScriptedEvent"/> we listen to.
+        /// </summary>
+        public ScriptedEvent<T> Event
+        {
+            get => this.@event;
+            set => this.@event = value;
+        }
+
+    }
+}
