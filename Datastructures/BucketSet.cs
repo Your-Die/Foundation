079e63acda3defdded3a9a421dd952a82eb1ae11
Crowcontrol <jordy_van_opstal@live.nl>
2020-03-06 11:33:00 +0100

    Make Bucketset.Buckify a constructor instead.

--- Datastructures/BucketSet.cs
+++ Datastructures/BucketSet.cs
@@ -12,11 +12,18 @@ namespace Chinchillada.Utilities
 
         public IEnumerable<TKey> Buckets => this.buckets.Keys;
 
+        public BucketSet(IEnumerable<TValue> collection, Func<TValue, TKey> bucketSelector)
+            : this(bucketSelector)
+        {
+            foreach (var item in collection)
+                this.Add(item);
+        }
+
         public BucketSet(Func<TValue, TKey> bucketSelector)
         {
             this.buckets = new DefaultDictionary<TKey, List<TValue>>(BucketConstructor);
             this.bucketSelector = bucketSelector;
-            
+
             List<TValue> BucketConstructor() => new List<TValue>();
         }
 
@@ -26,17 +33,6 @@ namespace Chinchillada.Utilities
             this.buckets[bucket].Add(value);
 
             return bucket;
-        }
-
-        public static BucketSet<TKey, TValue> Buckify(IEnumerable<TValue> collection,
-            Func<TValue, TKey> bucketSelector)
-        {
-            var bucketSet = new BucketSet<TKey, TValue>(bucketSelector);
-            
-            foreach (var item in collection) 
-                bucketSet.Add(item);
-
-            return bucketSet;
         }
     }
 }
