bff9d535afb67de0486d296498a5a33270572a50
Jordy <jordy_van_opstal@live.nl>
2020-03-24 16:30:45 +0100

    Add color extension method for TMP.

--- Helpers/TMPHelper.cs
+++ Helpers/TMPHelper.cs
@@ -1,5 +1,6 @@
 namespace Chinchillada.Utilities
 {
+    using System;
     using TMPro;
     using UnityEngine;
     using UnityEngine.EventSystems;
@@ -61,6 +62,45 @@ namespace Chinchillada.Utilities
             var linkInfo = text.textInfo.linkInfo[linkIndex];
             linkID = linkInfo.GetLinkID();
             return true;
+        }
+
+        public static int GetMouseoverLinkIndex(this TMP_Text text)
+        {
+            var position = Input.mousePosition;
+            var camera = Camera.main;
+
+            if (camera == null)
+                throw new InvalidOperationException();
+
+             return TMP_TextUtilities.FindIntersectingLink(text, position, camera);
+        }
+
+        public static void SetColor(this TMP_Text text, TMP_LinkInfo linkInfo, Color32 color,
+            bool updateVertexData = true)
+        {
+            var textInfo = text.textInfo;
+
+            for (var i = 0; i < linkInfo.linkTextLength; i++)
+            {
+                var characterIndex = linkInfo.linkTextfirstCharacterIndex + i;
+
+                // Get the index of the material / sub text object used by this character.
+                var meshIndex = textInfo.characterInfo[characterIndex].materialReferenceIndex;
+
+                // Get the index of the first vertex of this character.
+                var vertexIndex = textInfo.characterInfo[characterIndex].vertexIndex;
+
+                // Get a reference to the vertex color
+                var vertexColors = textInfo.meshInfo[meshIndex].colors32;
+
+                vertexColors[vertexIndex + 0] = color;
+                vertexColors[vertexIndex + 1] = color;
+                vertexColors[vertexIndex + 2] = color;
+                vertexColors[vertexIndex + 3] = color;
+            }
+
+            if (updateVertexData)
+                text.UpdateVertexData(TMP_VertexDataUpdateFlags.All);
         }
     }
 }
