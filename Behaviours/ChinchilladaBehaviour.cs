8b6bd93b7ebccdfe023311f77cfe077de9d1dddc
Jordy <jordy_van_opstal@live.nl>
2020-03-24 16:31:16 +0100

    Add OnActivate to ChinchilladaBehaviour: Triggered every OnEnable except the first, where it's triggered in Start.

--- Behaviours/ChinchilladaBehaviour.cs
+++ Behaviours/ChinchilladaBehaviour.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 namespace Chinchillada
 {
+    using System;
+
     /// <summary>
     /// Base class for Monobehaviours. Inherits from <see cref="Sirenix.OdinInspector.SerializedMonoBehaviour"/>.
     /// Automatically applies <see cref="FindComponentAttribute"/> on awake, and also extends a Button to manually trigger it from the Unity editor.
     /// </summary>
     public abstract class ChinchilladaBehaviour : SerializedMonoBehaviour, IComponent
     {
+        private bool hasStarted;
+        
         protected virtual void Awake()
         {
             this.FindComponents();
+        }
+
+        protected virtual void Start()
+        {
+            this.OnActivate();
+            this.hasStarted = true;
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (this.hasStarted) 
+                this.OnActivate();
+        }
+
+        protected virtual void OnActivate()
+        {
         }
         
         /// <summary>
