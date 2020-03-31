6887ee045f997c19446a3c04cec5aa4735b406a1
Jordy <jordy_van_opstal@live.nl>
2020-03-31 20:31:21 +0200

    Add some utility functions for RectTransform.

---
+++ Extensions/RectTransformExtensions.cs
@@ -0,0 +1,19 @@
+namespace Chinchillada.Utilities
+{
+    using UnityEngine;
+
+    public static class RectTransformExtensions
+    {
+        public static bool ScreenPointToWorldPoint(this RectTransform transform, Camera camera, Vector3 position,
+            out Vector3 point)
+        {
+            return RectTransformUtility.ScreenPointToWorldPointInRectangle(transform, position, camera, out point);
+        }
+
+        public static Camera GetCanvasCamera(this RectTransform transform)
+        {
+            var canvas = transform.GetComponentInParent<Canvas>();
+            return canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera;
+        }
+    }
+}
