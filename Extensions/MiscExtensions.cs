a4870b922a11cae72194db36ddbf4a4a74a649cb
Jordy <jordy_van_opstal@live.nl>
2020-04-13 15:15:57 +0200

    Add IsEven extension for ints.

--- Extensions/MiscExtensions.cs
+++ Extensions/MiscExtensions.cs
@@ -25,7 +25,13 @@ namespace Chinchillada.Utilities
 
         public static bool IsUneven(this int value)
         {
-            return value % 2 != 0;
+            return value.IsEven() == false;
+        }
+
+        public static bool IsEven(this int value)
+        {
+            return value % 2 == 0;
+
         }
         
         public static (int width, int height) GetShape<T>(this T[,] array)
