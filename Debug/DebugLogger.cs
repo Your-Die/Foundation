983916ce5dfefe88a814a5088d957e21aede1aa5
Jordy <jordy_van_opstal@live.nl>
2020-04-19 14:32:52 +0200

    add debug logger.

---
+++ Debug/DebugLogger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class DebugLogger : MonoBehaviour
+{
+    [SerializeField] private string message;
+
+    [UsedImplicitly]
+    public void LogFieldMessage() => this.LogMessage(this.message);
+
+    public void LogMessage(string message)
+    {
+        Debug.Log(message);
+    }
+}
