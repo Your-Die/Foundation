62c94a921c2c6655b969331867942c6e6215be52
Jordy <jordy_van_opstal@live.nl>
2020-04-29 10:26:12 +0200

    Add Mouse event.

---
+++ Events/Input/MouseEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mutiny.Foundation.Events
+{
+    public class MouseEvent : SimpleEvent
+    {
+        [SerializeField] private int mouseButton = 0;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(this.mouseButton)) 
+                this.Event.Invoke();
+        }
+    }
+}
