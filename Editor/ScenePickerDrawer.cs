96d8575f811dee40a2f3f9e537652c1d0cea6c01
Jordy <jordy_van_opstal@live.nl>
2020-05-01 11:41:16 +0200

    Import scene picker attribute.

---
+++ Editor/ScenePickerDrawer.cs
@@ -0,0 +1,26 @@
+using Chinchillada.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mutiny.Foundation.Editor
+{
+    [CustomPropertyDrawer(typeof(ScenePickerAttribute))]
+    public class ScenePickerDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var oldPath = AssetDatabase.LoadAssetAtPath<SceneAsset>(property.stringValue);
+
+            position = EditorGUI.PrefixLabel(position, new GUIContent(property.displayName));
+
+            EditorGUI.BeginChangeCheck();
+            var newScene = EditorGUI.ObjectField(position, oldPath, typeof(SceneAsset), false) as SceneAsset;
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                var newPath = AssetDatabase.GetAssetPath(newScene);
+                property.stringValue = newPath;
+            }
+        }
+    }
+}
