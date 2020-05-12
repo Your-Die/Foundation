c7d5ea2c9410cb85f6be39b86719dcba6e253656
Jordy <jordy_van_opstal@live.nl>
2020-05-13 00:53:52 +0200

    Add bucket collection.

---
+++ Datastructures/BucketCollection.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Chinchillada.Utilities
+{
+    public class BucketCollection<TKey, TValue> : IDictionary<TKey, ICollection<TValue>>
+    {
+        private readonly IDictionary<TKey, ICollection<TValue>> buckets;
+        private IDictionary<TKey, ICollection<TValue>> dictionaryImplementation;
+
+        public int Count => this.buckets.Count;
+
+        public bool IsReadOnly => this.buckets.IsReadOnly;
+        
+        public bool TryGetValue(TKey key, out ICollection<TValue> value)
+        {
+            return this.buckets.TryGetValue(key, out value);
+        }
+
+        public ICollection<TValue> this[TKey key]
+        {
+            get => this.buckets[key];
+            set => this.buckets[key] = value;
+        }
+
+        public ICollection<TKey> Keys => this.buckets.Keys;
+
+        public ICollection<ICollection<TValue>> Values => this.buckets.Values;
+
+        public BucketCollection() : this(() => new List<TValue>())
+        {
+        }
+
+        public BucketCollection(Func<ICollection<TValue>> collectionFactory)
+        {
+            this.buckets = new DefaultDictionary<TKey, ICollection<TValue>>(collectionFactory);
+        }
+
+        public BucketCollection(IDictionary<TKey, ICollection<TValue>> buckets)
+        {
+            this.buckets = buckets;
+        }
+
+        public void Add(TKey key, TValue value) => this.buckets[key].Add(value);
+
+        public bool Remove(TKey key) => this.buckets.Remove(key);
+
+        public bool Remove(TKey key, TValue value)
+        {
+            var bucket = this.buckets[key];
+            return bucket != null && bucket.Remove(value);
+        }
+
+        public void Add(TKey key, ICollection<TValue> value) => this.buckets.Add(key, value);
+
+        public void Clear() => this.buckets.Clear();
+
+        public bool ContainsKey(TKey key) => this.buckets.ContainsKey(key);
+        
+        public IEnumerator<KeyValuePair<TKey, ICollection<TValue>>> GetEnumerator() => this.buckets.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) this.buckets).GetEnumerator();
+
+        public void Add(KeyValuePair<TKey, ICollection<TValue>> item) => this.buckets.Add(item);
+
+        public bool Contains(KeyValuePair<TKey, ICollection<TValue>> item) => this.buckets.Contains(item);
+
+        public void CopyTo(KeyValuePair<TKey, ICollection<TValue>>[] array, int arrayIndex)
+        {
+            this.buckets.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<TKey, ICollection<TValue>> item) => this.buckets.Remove(item);
+
+
+    }
+}
