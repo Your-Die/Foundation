e1cded024a39eb64fa58d2918136d5201713ac57
Jordy <jordy_van_opstal@live.nl>
2020-05-20 13:58:26 +0200

    Add Transform move Tween component.

---
+++ Common/TransformMoveTween.cs
@@ -0,0 +1,37 @@
+﻿using Chinchillada;
+using Chinchillada.Utilities;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Mutiny.Foundation.Common
+{
+    public class TransformMoveTween : ChinchilladaBehaviour
+    {
+        [SerializeField, FindComponent] private Transform target;
+
+        [SerializeField] private Transform endLocation;
+
+        [SerializeField] private float duration;
+
+        [SerializeField] private Ease ease;
+
+        private Tweener tweener;
+        
+        public Tweener Tween()
+        {
+            this.Kill();
+            this.tweener = this.target.DOMove(this.endLocation.position, this.duration).SetEase(this.ease);
+
+            return this.tweener;
+        }
+
+        public void CompleteImmediate()
+        {
+            this.Kill();
+            
+            this.target.position = this.endLocation.position;
+        }
+
+        public void Kill() => this.tweener?.Kill();
+    }
+}
