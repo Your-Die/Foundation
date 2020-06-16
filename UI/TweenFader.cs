adab656ad1a8b02270ca1a9364ec6c8c9310219f
Jordy <jordy_van_opstal@live.nl>
2020-06-16 13:20:26 +0200

    Add tween fader.

---
+++ UI/TweenFader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using Chinchillada.Foundation;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Mutiny.UI
+{
+    public class TweenFader : ChinchilladaBehaviour
+    {
+        [SerializeField, FindComponent(SearchStrategy.InChildren)]
+        private Image fadeImage;
+
+        [SerializeField] private FadeTween fadeIn;
+
+        [SerializeField] private FadeTween fadeOut;
+
+        private Tweener tweener;
+
+        private IEnumerator routine;
+
+        [Button]
+        public void StartFadeIn() => this.StartFade(this.FadeIn());
+
+        [Button]
+        public void StartFadeOut() => this.StartFade(this.FadeOut());
+
+        public IEnumerator FadeIn() => this.Fade(this.fadeIn);
+
+        public IEnumerator FadeOut() => this.Fade(this.fadeOut);
+
+        private IEnumerator Fade(FadeTween fadeTweener)
+        {
+            this.tweener = fadeTweener.DoTween(this.fadeImage);
+            yield return this.tweener.WaitForCompletion();
+        }
+
+        private void StartFade(IEnumerator fadeRoutine)
+        {
+            this.tweener?.Kill();
+
+            if (this.routine != null)
+                this.StopCoroutine(this.routine);
+
+            this.routine = fadeRoutine;
+            this.StartCoroutine(this.routine);
+        }
+
+        [Serializable]
+        private class FadeTween
+        {
+            [SerializeField] private float duration = 0.3f;
+
+            [SerializeField] private Color targetColor = Color.black;
+
+            [SerializeField] private Ease ease;
+
+            [SerializeField] private UnityEvent startEvent;
+            [SerializeField] private UnityEvent completionEvent;
+            
+            public Tweener DoTween(Image image)
+            {
+                this.startEvent.Invoke();
+                
+                var tweener =image.DOColor(this.targetColor, this.duration).SetEase(this.ease);
+                tweener.onComplete = this.OnComplete;
+
+                return tweener;
+            }
+
+            private void OnComplete() => this.completionEvent?.Invoke();
+        }
+    }
+}
