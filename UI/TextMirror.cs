64a45262a6ae3ec411af4928d11cb55fcf57c724
Jordy <jordy_van_opstal@live.nl>
2020-06-16 12:21:42 +0200

    Add TextMirror.

---
+++ UI/TextMirror.cs
@@ -0,0 +1,22 @@
+﻿using Chinchillada.Foundation;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+public class TextMirror : MonoBehaviour
+{
+    [SerializeField, Required] private TMP_Text toMirror;
+    [SerializeField, Required, FindComponent] private TMP_Text mirroringText;
+
+    [Button]
+    private void MirrorText()
+    {
+        this.mirroringText.text = this.toMirror.text;
+    }
+    
+    private void OnEnable() => this.toMirror.RegisterDirtyVerticesCallback(this.MirrorText);
+
+    private void OnDisable() => this.toMirror.UnregisterDirtyVerticesCallback(this.MirrorText);
+
+
+}
