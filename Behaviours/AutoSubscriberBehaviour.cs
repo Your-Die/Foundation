2bd00a51ad74c9aba99211c61168f5f301aa354c
Jordy <jordy_van_opstal@live.nl>
2020-06-23 16:50:41 +0200

    Add subscriber behaviours.

---
+++ Behaviours/AutoSubscriberBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Chinchillada.Foundation
+{
+    public abstract class AutoSubscriberBehaviour : SubscriberBehaviour
+    {
+        private SubscriptionStrategy strategy;
+
+        protected virtual SubscriptionStrategy Strategy => this.strategy;
+
+        protected virtual void OnEnable()
+        {
+            if (this.Strategy == SubscriptionStrategy.EnableDisable)
+                this.Subscribe();
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (this.Strategy == SubscriptionStrategy.EnableDisable)
+                this.Unsubscribe();
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            if (this.Strategy == SubscriptionStrategy.AwakeDestroy)
+                this.Subscribe();
+        }
+
+        protected virtual void Start()
+        {
+            if (this.Strategy == SubscriptionStrategy.StartDestroy) 
+                this.Subscribe();
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (this.Strategy == SubscriptionStrategy.AwakeDestroy || 
+                this.Strategy == SubscriptionStrategy.StartDestroy)
+                this.Unsubscribe();
+        }
+    }
+
+    public enum SubscriptionStrategy
+    {
+        Manual,
+        EnableDisable,
+        AwakeDestroy,
+        StartDestroy
+    }
+}
---
+++ Behaviours/SubscriberBehaviour.cs
@@ -0,0 +1,30 @@
+﻿namespace Chinchillada.Foundation
+{
+    public abstract class SubscriberBehaviour : ChinchilladaBehaviour
+    {
+        public bool IsSubscribed { get; private set; }
+
+        public void Subscribe()
+        {
+            if (this.IsSubscribed)
+                return;
+
+            this.ActivateSubscriptions();
+
+            this.IsSubscribed = true;
+        }
+
+        public void Unsubscribe()
+        {
+            if (!this.IsSubscribed)
+                return;
+
+            this.DeactivateSubscriptions();
+
+            this.IsSubscribed = false;
+        }
+
+        protected abstract void ActivateSubscriptions();
+        protected abstract void DeactivateSubscriptions();
+    }
+}
