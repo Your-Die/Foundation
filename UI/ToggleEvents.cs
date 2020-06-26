88108480efb99a718582a68fa1225da219fde6b5
Jordy <jordy_van_opstal@live.nl>
2020-06-26 16:23:17 +0200

    Add toggle events.

---
+++ UI/ToggleEvents.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Chinchillada.Foundation;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Mutiny.Foundation.UI
+{
+    public class ToggleEvents : ChinchilladaBehaviour
+    {
+        [SerializeField, Required, FindComponent(SearchStrategy.InChildren)]
+        private Toggle toggle;
+
+        [SerializeField] private bool fireOnStart;
+
+        [SerializeField] private UnityEvent onTrue;
+
+        [SerializeField] private UnityEvent onFalse;
+
+
+        [Button]
+        public void InvokeCurrent()
+        {
+            var current = this.toggle.isOn;
+            this.InvokeEvent(current);
+        }
+
+        private void InvokeEvent(bool value)
+        {
+            var @event = value ? this.onTrue : this.onFalse;
+            @event.Invoke();
+        }
+
+        private void Start()
+        {
+            if (this.fireOnStart)
+                this.InvokeCurrent();
+        }
+
+        private void OnEnable() => this.toggle.onValueChanged.AddListener(this.InvokeEvent);
+
+        private void OnDisable() => this.toggle.onValueChanged.RemoveListener(this.InvokeEvent);
+    }
+}
