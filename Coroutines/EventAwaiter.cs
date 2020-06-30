146f76c8a06ae6d18feac6fbca08789bd7a004c4
Jordy <jordy_van_opstal@live.nl>
2020-06-30 15:17:12 +0200

    Implement awaiters.

---
+++ Coroutines/EventAwaiter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Mutiny.Foundation.Common;
+using UnityEngine;
+
+namespace Mutiny.Foundation.Coroutines
+{
+    public class EventAwaiter : CustomYieldInstruction
+    {
+        private bool isEventTriggered;
+
+        public override bool keepWaiting => !this.isEventTriggered;
+
+
+        public EventAwaiter(Action<Action> subscribe, Action<Action> unsubscribe)
+        {
+            OneTimeSubscriber.CreateAndSubscribe(this.OnEventTriggered, subscribe, unsubscribe);
+        }
+
+        private void OnEventTriggered() => this.isEventTriggered = true;
+    }
+}
---
+++ Coroutines/UnityEventAwaiter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Mutiny.Foundation.Coroutines
+{
+    public class UnityEventAwaiter : CustomYieldInstruction
+    {
+        private bool isEventTriggered;
+
+        public override bool keepWaiting => !this.isEventTriggered;
+
+        private readonly UnityEvent @event;
+        
+        public UnityEventAwaiter(UnityEvent @event)
+        {
+            this.@event = @event;
+            @event.AddListener(this.OnEventTriggered);
+        }
+
+        private void OnEventTriggered()
+        {
+            this.@event.RemoveListener(this.OnEventTriggered);
+            this.isEventTriggered = true;
+        }
+    }
+}
