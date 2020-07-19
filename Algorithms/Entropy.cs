a6b036c0c5e6b26697a214c468ec473407a66e23
Jordy <jordy_van_opstal@live.nl>
2020-07-19 16:20:05 +0200

    Add Shannon entropy.

---
+++ Algorithms/Entropy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foundation.Algorithms
+{
+    public static class Entropy
+    {
+        public static float Shannon(IEnumerable<float> weights)
+        {
+            var sum = 0f;
+            var loggedSum = 0f;
+
+            foreach (var weight in weights)
+            {
+                var weightLog = Mathf.Log(weight);
+                
+                sum += weight;
+                loggedSum += weight * weightLog;
+            }
+
+            return Mathf.Log(sum) - loggedSum / sum;
+        }
+    }
+}
