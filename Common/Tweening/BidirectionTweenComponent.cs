99e86831ccdd85c0311c8afcf80dc4c5bcf0542a
Jordy <jordy_van_opstal@live.nl>
2020-07-28 09:26:39 +0200

    Add BidirectionTweenComponent

---
+++ Common/Tweening/BidirectionTweenComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Chinchillada.Foundation
+{
+    public class BidirectionTweenComponent : ChinchilladaBehaviour, IBidirectionalTweener
+    {
+        [SerializeField] private IBidirectionalTweener tweener;
+        public void TweenForward() => this.tweener.TweenForward();
+
+        public void TweenBackward() => this.tweener.TweenBackward();
+
+        public void ForceForward()
+        {
+            this.tweener.ForceForward();
+        }
+
+        public void ForceBackward()
+        {
+            this.tweener.ForceBackward();
+        }
+    }
+}
