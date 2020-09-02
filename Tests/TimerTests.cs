f923c6c11a7032ffe85e896c58b16804d46c6724
Jordy <jordy_van_opstal@live.nl>
2020-09-02 14:50:15 +0200

    Add timer tests.

---
+++ Tests/TimerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using Chinchillada.Timers;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Chinchillada.Foundation.Tests
+{
+    public static class TimerTests
+    {
+        private static float[] durationValues = {0, 0.1f, 0.001f, 0.0001f, 1, 3.2f};
+
+        [UnityTest]
+        public static IEnumerator StartTimer_IsFinished_AfterDuration(
+            [ValueSource(nameof(durationValues))] float duration)
+        {
+            var timer = new Timer(duration);
+
+            timer.Start();
+            yield return new WaitForSeconds(duration);
+
+            Assert.That(timer.IsRunning, Is.False);
+        }
+
+        [UnityTest]
+        public static IEnumerator StartTimer_CallbackInvoked_AfterDuration(
+            [ValueSource(nameof(durationValues))] float duration)
+        {
+            var isCalled = false;
+            var timer = new Timer(duration, () => isCalled = true);
+
+            timer.Start();
+            yield return new WaitForSeconds(duration);
+
+            Assert.That(isCalled, Is.True);
+        }
+    }
+}
---
+++ Timers/TimerAwaiter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Chinchillada.Timers
+{
+    public class TimerAwaiter : CustomYieldInstruction
+    {
+        private readonly ITimer timer;
+        
+        public override bool keepWaiting => this.timer.IsRunning;
+        
+        public TimerAwaiter(ITimer timer)
+        {
+            this.timer = timer;
+        } 
+    }
+}
