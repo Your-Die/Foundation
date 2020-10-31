f1f2cddce47a458ec030e93d30d8880c61ed742e
Jordy <jordy_van_opstal@live.nl>
2020-10-31 19:37:17 +0100

    Remove warning from FindNestedComponentsAttribute

--- Runtime/Attributes/FindNestedComponentsAttribute.cs
+++ Runtime/Attributes/FindNestedComponentsAttribute.cs
@@ -10,16 +10,10 @@ namespace Chinchillada.Foundation
         public override void Apply(MonoBehaviour behaviour, object obj, FieldInfo field)
         {
             if (typeof(Object).IsAssignableFrom(field.FieldType))
-            {
-                var message = $"{nameof(FindNestedComponentsAttribute)} found on a type that derives from {nameof(Object)}: " +
-                              $"{Environment.NewLine}" +
-                              $"{nameof(MonoBehaviour)}: {behaviour} - Field: {field}";
-               
-                Debug.LogWarning(message);
                 return;
-            }
 
             var nestedObject = field.GetValue(obj);
+            
             var nestedFields = AttributeHelper.GetAttributedFields<ComponentFinderAttribute>(nestedObject);
             
             foreach (var (nestedField, attribute) in nestedFields)
