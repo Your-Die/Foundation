0d07d60dbeab94c3bfdbbd24f09179b6fb52cc1a
Jordy <jordy_van_opstal@live.nl>
2020-12-08 23:28:46 +0100

    add default value to the Rng.flip

--- Runtime/Random/IRNG.cs
+++ Runtime/Random/IRNG.cs
@@ -2,7 +2,7 @@
 {
     public interface IRNG
     {
-        bool Flip(float probability);
+        bool Flip(float probability = 0.5f);
         int Range(int min, int max, bool inclusive = false);
     }
 }
