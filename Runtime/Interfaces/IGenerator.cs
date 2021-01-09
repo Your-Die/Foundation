117a39c3c82ab28cb11d4b1f68e2cf779e571aa8
Jordy <jordy_van_opstal@live.nl>
2021-01-09 22:55:40 +0100

    Directorize interfaces

---
+++ Runtime/Interfaces/IGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chinchillada
+{
+    public interface IGenerator<T>
+    {
+        T Result { get; }
+        event Action<T> Generated;
+        T Generate();
+    }
+    public static class GeneratorExtensions
+    {
+        public static IEnumerable<T> Generate<T>(this IGenerator<T> generator, int amount)
+        {
+            for (var i = 0; i < amount; i++)
+                yield return generator.Generate();
+        }
+    }}
