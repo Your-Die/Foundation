7d83d97e930e550678deecfdb339d0e2284e1c8f
Jordy <jordy_van_opstal@live.nl>
2021-01-10 15:05:39 +0100

    Make attributehelper.GetAllFields include base-classes

--- Runtime/Helpers/AttributeHelper.cs
+++ Runtime/Helpers/AttributeHelper.cs
@@ -15,27 +15,52 @@ namespace Chinchillada.Foundation
         public static IEnumerable<(FieldInfo field, TAttribute)> GetAttributedFields<TAttribute>(object obj)
             where TAttribute : PropertyAttribute
         {
-            var fields = GetAllFields(obj);
-            var attributeType = typeof(TAttribute);
+            var type = obj.GetType();
+            var fields = GetAllFields(type);
 
             foreach (var field in fields)
             {
-                var attributes = field.GetCustomAttributes(attributeType).ToList();
+                var value = field.GetValue(obj);
+                if (!IsNull(value))
+                    continue;
+
+                var attributes = field.GetCustomAttributes(typeof(TAttribute)).ToList();
                 if (attributes.IsEmpty())
                     continue;
 
-                var attribute = (TAttribute)attributes.First();
-                yield return (field, attribute);
+                var attribute = (TAttribute) attributes.First();
+                yield return(field, attribute);
             }
         }
 
-        private static IEnumerable<FieldInfo> GetAllFields(object obj)
+        public static IEnumerable<FieldInfo> GetAllFields(Type type)
         {
-            const BindingFlags bindingFlags = BindingFlags.Instance |
-                                              BindingFlags.NonPublic |
+            const BindingFlags bindingFlags = BindingFlags.Instance     |
+                                              BindingFlags.DeclaredOnly |
+                                              BindingFlags.NonPublic    |
                                               BindingFlags.Public;
-            var type = obj.GetType();
-            return type.GetFields(bindingFlags);
+
+            var types = GetBaseClasses(type, true);
+            return types.SelectMany(baseType => baseType.GetFields(bindingFlags));
+        }
+
+        public static IEnumerable<Type> GetBaseClasses(Type type, bool includeSelf = false)
+        {
+            if (includeSelf)
+                yield return type;
+
+            for (var current = type.BaseType; current != null; current = current.BaseType)
+                yield return current;
+        }
+
+        private static bool IsNull(object item)
+        {
+            switch (item)
+            {
+                case null: return true;
+                case Object unityObject: return unityObject == null;
+                default: return false;
+            }
         }
     }
 }
