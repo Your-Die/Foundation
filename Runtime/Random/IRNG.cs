e02905eaea9664883b911dc0147126efab45583b
Jordy <jordy_van_opstal@live.nl>
2021-01-10 01:14:19 +0100

    Add IRNG.Range without min

--- Runtime/Random/IRNG.cs
+++ Runtime/Random/IRNG.cs
@@ -5,4 +5,12 @@
         bool Flip(float probability = 0.5f);
         int Range(int min, int max, bool inclusive = false);
     }
+
+    public static class RNGExtensions
+    {
+        public static int Range(this IRNG rng, int max, bool inclusive = false)
+        {
+            return rng.Range(0, max, inclusive);
+        }
+    }
 }
