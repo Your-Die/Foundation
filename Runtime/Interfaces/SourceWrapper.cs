584c84945d36fd7c69c0a6ea93865c94ecc15f29
Jordy <jordy_van_opstal@live.nl>
2021-01-17 20:49:51 +0100

    make SourceWrapper Odin-serialize it's item

--- Runtime/Interfaces/SourceWrapper.cs
+++ Runtime/Interfaces/SourceWrapper.cs
@@ -2,12 +2,12 @@
 {
     using System;
     using Chinchillada.Foundation;
-    using UnityEngine;
+    using Sirenix.Serialization;
 
     [Serializable]
     public class SourceWrapper<T> : ISource<T>
     {
-        [SerializeField] private T item;
+        [OdinSerialize] private T item;
 
         public SourceWrapper() => this.item = default;
         
