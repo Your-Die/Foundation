f4ac7c37ca1f340f37c811f53629de4c67f36490
Jordy <jordy_van_opstal@live.nl>
2021-01-17 16:18:06 +0100

    Add RNG component

---
+++ Runtime/Random/RNGComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace Chinchillada
+{
+    using Foundation;
+    using UnityEngine;
+
+    public class RNGComponent : ChinchilladaBehaviour, IRNG
+    {
+        [SerializeField] private IRNG random;
+
+        public void SetSeed(int seed) => this.random.SetSeed(seed);
+
+        public float Float() => this.random.Float();
+
+        public float Range(float min, float max) => this.random.Range(min, max);
+
+        public int Range(int min, int max, bool inclusive = false) => this.random.Range(min, max, inclusive);
+
+        public void Initialize() => this.random.Initialize();
+    }
+}
