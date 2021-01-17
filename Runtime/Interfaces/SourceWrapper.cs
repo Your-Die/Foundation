740341afd97d6020395716b0a6d856abb3b214a9
Jordy <jordy_van_opstal@live.nl>
2021-01-17 16:18:12 +0100

    Add SourceWrapper

---
+++ Runtime/Interfaces/SourceWrapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Interfaces
+{
+    using System;
+    using Chinchillada.Foundation;
+    using UnityEngine;
+
+    [Serializable]
+    public class SourceWrapper<T> : ISource<T>
+    {
+        [SerializeField] private T item;
+
+        public SourceWrapper() => this.item = default;
+        
+        public SourceWrapper(T item) => this.item = item;
+
+        public T GetValue() => this.item;
+    }
+}
