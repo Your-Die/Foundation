5e28cfd6876d584bc87465f3bfe26e69d9c61a65
Jordy <jordy_van_opstal@live.nl>
2021-01-25 01:11:57 +0100

    Add DeferredPoolList

---
+++ Runtime/Datastructures/Collections/Pooling List/DeferredPoolList.cs
@@ -0,0 +1,11 @@
+﻿namespace Chinchillada.Foundation
+{
+    using UnityEngine;
+
+    public class DeferredPoolList<T> : PoolListBase<T>
+    {
+        [SerializeField] private IGameObjectPool pool;
+        
+        protected override T CreateNew() => this.pool.Instantiate<T>();
+    }
+}
