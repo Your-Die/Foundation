e6dee15f39e9689c65d1f8c93abe7f73e3b39927
Jordy <jordy_van_opstal@live.nl>
2021-02-07 20:06:32 +0100

    Add OneTimeListener

---
+++ Runtime/Coroutines/OneTimeListener.cs
@@ -0,0 +1,40 @@
+﻿namespace Coroutines
+{
+    using UnityEngine.Events;
+
+    public class OneTimeListener
+    {
+        private readonly UnityEvent @event;
+        private readonly UnityAction action;
+
+        public static OneTimeListener Create(UnityEvent @event, UnityAction action)
+        {
+            var subscriber = new OneTimeListener(@event, action);
+            subscriber.Subscribe();
+
+            return subscriber;
+        }
+        
+        public OneTimeListener(UnityEvent @event, UnityAction action)
+        {
+            this.@event = @event;
+            this.action = action;
+        }
+
+        public void Subscribe()
+        {
+            this.@event.AddListener(this.OnEventInvoked);
+        }
+
+        public void Unsubscribe()
+        {
+            this.@event.RemoveListener(this.OnEventInvoked);
+        }
+
+        private void OnEventInvoked()
+        {
+            this.Unsubscribe();
+            this.action.Invoke();
+        }
+    }
+}
