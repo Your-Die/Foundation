416ad6e67f688690b593f38bc0736e52e27fd987
Jordy <jordy_van_opstal@live.nl>
2021-02-14 03:09:31 +0100

    Add string.Decapitalize

---
+++ Runtime/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Chinchillada.Foundation
+{
+    public static class StringExtensions
+    {
+        public static string Decapitalize(this string text)
+        {
+            if (text.Length == 0)
+                return text;
+
+            var firstLetter = text[0];
+            if (char.IsLower(firstLetter))
+                return text;
+
+            var lower = char.ToLower(firstLetter);
+            return lower + text.Substring(1);
+        }
+    }
+}
