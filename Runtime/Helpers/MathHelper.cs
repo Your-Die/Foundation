485e91c26932983dee8379d5ce1530e6a1a488fb
Jordy <jordy_van_opstal@live.nl>
2021-02-28 20:40:34 +0100

    Add circle line intersection to MathHelper

--- Runtime/Helpers/MathHelper.cs
+++ Runtime/Helpers/MathHelper.cs
@@ -137,5 +137,60 @@ namespace Chinchillada.Foundation
 
             return s >= 0 && s <= 1 && t >= 0 && t <= 1;
         }
+
+        public static bool CircleLineIntersection(Vector2 circleCenter, float circleRadius,
+                                                  Vector2 lineStart,    Vector2 lineEnd)
+        {
+            var delta    = lineEnd   - lineStart;
+            var distance = lineStart - circleCenter;
+
+            var a = Vector2.Dot(delta, delta );
+            var b = 2 * Vector2.Dot(distance, delta );
+            var c = Vector2.Dot(distance,     distance) - circleRadius * circleRadius;
+
+            var discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
+                return false;
+
+            // ray didn't totally miss sphere,
+            // so there is a solution to
+            // the equation.
+
+            discriminant = Mathf.Sqrt( discriminant );
+
+            // either solution may be on or off the ray so need to test both
+            // t1 is always the smaller value, because BOTH discriminant and
+            // a are nonnegative.
+            var t1 = (-b - discriminant) / (2 * a);
+            var t2 = (-b + discriminant) / (2 * a);
+
+            // 3x HIT cases:
+            //          -o->             --|-->  |            |  --|->
+            // Impale(t1 hit,t2 hit), Poke(t1 hit,t2>1), ExitWound(t1<0, t2 hit), 
+
+            // 3x MISS cases:
+            //       ->  o                     o ->              | -> |
+            // FallShort (t1>1,t2>1), Past (t1<0,t2<0), CompletelyInside(t1<0, t2>1)
+
+        
+            // t1 is the intersection, and it's closer than t2
+            // (since t1 uses -b - discriminant)
+            // Impale, Poke
+            if ( t1 >= 0 && t1 <= 1 )
+                return true;
+
+            // here t1 didn't intersect so we are either started
+            // inside the sphere or completely past it
+            // ExitWound
+            if ( t2 >= 0 && t2 <= 1 )
+                return true ;
+
+            // CompletelyInside
+            if (t1 < 0 && t2 > 1)
+                return true;
+
+            // no intn: FallShort, Past, CompletelyInside
+            return false;
+        }
     }
 }
