da77a914626735efad7db2adad600ba0e0e9f8ff
jordy <jordy_van_opstal@live.nl>
2021-10-22 17:42:03 +0200

    Add IEnumerable<ICondition>.ValidateAll()

--- Runtime/Behaviour Selection/ICondition.cs
+++ Runtime/Behaviour Selection/ICondition.cs
@@ -1,5 +1,8 @@
 ﻿namespace Chinchillada.Behavior
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public interface ICondition
     {
         bool Validate();
@@ -8,5 +11,13 @@
     public interface ICondition<in T>
     {
         bool Validate(T value);
+    }
+
+    public static class ConditionExtensions
+    {
+        public static bool ValidateAll(this IEnumerable<ICondition> conditions)
+        {
+            return conditions.All(condition => condition.Validate());
+        }
     }
 }
