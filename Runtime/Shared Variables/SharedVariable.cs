ce18f1b0958df10ca436f33c7b8e05f3c716cc6e
jordy <jordy_van_opstal@live.nl>
2021-11-26 21:19:09 +0100

    Hook SharedVariable into odin

--- Runtime/Shared Variables/SharedVariable.cs
+++ Runtime/Shared Variables/SharedVariable.cs
@@ -9,7 +9,7 @@ namespace Chinchillada
     /// A variable of the given type that is wrapped in a <see cref="ScriptableObject"/> so it can be easily shared by different systems
     /// in a modular way.
     /// </summary> 
-    public abstract class SharedVariable<T> : ScriptableObject, IVariable<T>, IContainer<T>,
+    public abstract class SharedVariable<T> : SerializedScriptableObject, IVariable<T>, IContainer<T>,
                                               ISerializationCallbackReceiver
         where T : IComparable
     {
