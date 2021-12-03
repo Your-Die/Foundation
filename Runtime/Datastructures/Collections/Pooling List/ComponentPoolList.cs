d875c5b34b50b72d3c510b7beacdd04961f72a41
jordy <jordy_van_opstal@live.nl>
2021-12-03 19:21:11 +0100

    Remove duplicates of pooling lists

---
+++ Runtime/Datastructures/Collections/Pooling List/ComponentPoolList.cs
@@ -0,0 +1,36 @@
+﻿namespace Chinchillada
+{
+    using System;
+    using UnityEngine;
+    using Object = UnityEngine.Object;
+
+    [Serializable]
+    public class ComponentPoolList<TItem> : PoolListBase<TItem> where TItem : Component
+    {
+        [SerializeField] private TItem     prefab;
+        [SerializeField] private Transform parent;
+
+        protected TItem Prefab => this.prefab;
+
+        protected Transform Parent => this.parent;
+
+        public ComponentPoolList()
+        {
+        }
+
+        public ComponentPoolList(TItem prefab, Transform parent)
+        {
+            this.prefab = prefab;
+            this.parent = parent;
+        }
+        
+        protected override void Activate(TItem item) => item.gameObject.SetActive(true);
+
+        protected override void Deactivate(TItem item) => item.gameObject.SetActive(false);
+
+        protected override TItem CreateNew()
+        {
+            return Object.Instantiate(this.Prefab, this.Parent);
+        }
+    }
+}
