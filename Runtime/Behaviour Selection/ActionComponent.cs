457830ac994520491f4c82fe8ec2ea329d8864f1
jordy <jordy_van_opstal@live.nl>
2021-12-23 23:39:29 +0100

    remove FindNestedComponents attribute to ActionComponent.action

--- Runtime/Behaviour Selection/ActionComponent.cs
+++ Runtime/Behaviour Selection/ActionComponent.cs
@@ -5,7 +5,7 @@
 
     public class ActionComponent : ActionComponentBase
     {
-        [OdinSerialize, Required, FindNestedComponents] private IAction action;
+        [OdinSerialize, Required] private IAction action;
 
         public IAction Action
         {
