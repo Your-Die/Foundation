6d6175ea6845de20392af91c9d34ebf977f0b47d
jordy <jordy_van_opstal@live.nl>
2022-06-03 14:49:48 +0200

    add distribution factory

--- Runtime/Random/Distributions/IDistribution.cs
+++ Runtime/Random/Distributions/IDistribution.cs
@@ -1,7 +1,14 @@
 namespace Chinchillada
 {
+    using System.Collections.Generic;
+
     public interface IDistribution<out T>
     {
         public T Sample(IRNG random);
+    }
+    
+    public interface IDistributionFactory
+    {
+        IDistribution<T> BuildDistribution<T>(IDictionary<T, float> weighted);
     }
 }
