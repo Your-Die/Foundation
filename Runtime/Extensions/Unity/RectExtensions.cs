9a534562aefe12192b8d3eeac095554f6c37145b
jordy <jordy_van_opstal@live.nl>
2022-09-04 22:28:04 +0200

    add more rect utils

--- Runtime/Extensions/Unity/RectExtensions.cs
+++ Runtime/Extensions/Unity/RectExtensions.cs
@@ -2,6 +2,7 @@ namespace Chinchillada
 {
     using System;
     using System.Collections.Generic;
+    using System.Xml;
     using UnityEngine;
 
     public static class RectExtensions
@@ -33,6 +34,41 @@ namespace Chinchillada
             var rightRect = new Rect(splitPoint, rect.yMin, rect.xMax - splitPoint, rect.height);
 
             return (leftRect, rightRect);
+        }
+
+        public static float DistanceTo(this Rect rect, Rect other)
+        {
+            var boundingRect   = rect.BoundingBoxWith(other);
+            var projectingRect = rect.ProjectTo(other);
+
+            // Combined width/height is smaller if they overlap.
+            var overlapWidth  = boundingRect.width  < rect.width  + other.width;
+            var overlapHeight = boundingRect.height < rect.height + other.height;
+            
+            // If both overlap, the rects are intersecting.
+            if (overlapWidth && overlapHeight)
+                return 0f;
+
+            if (overlapWidth)
+                return Mathf.Abs(projectingRect.height);
+
+            if (overlapHeight)
+                return Mathf.Abs(projectingRect.width);
+
+            return Vector2.Distance(projectingRect.min, projectingRect.max);
+        }
+
+        public static Rect BoundingBoxWith(this Rect rect, Rect other)
+        {
+            var xMin = Mathf.Min(rect.xMin, other.xMin);
+            var yMin = Mathf.Min(rect.yMin, other.yMin);
+            var xMax = Mathf.Max(rect.xMax, other.xMax);
+            var yMax = Mathf.Max(rect.yMax, other.yMax);
+
+            var width  = xMax - xMin;
+            var height = yMax - xMin;
+
+            return new Rect(xMin, yMin, width, height);
         }
 
         public static Rect ProjectTo(this Rect from, Rect to)
