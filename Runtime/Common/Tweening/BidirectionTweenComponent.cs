de254e67eaee7bac6f223eca70550f6bfb66aec9
Jordy van Opstal <jordy_van_opstal@live.nl>
2023-03-18 19:29:39 +0100

    Delete Containers

--- Runtime/Common/Tweening/BidirectionTweenComponent.cs
+++ Runtime/Common/Tweening/BidirectionTweenComponent.cs
@@ -1,11 +1,13 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Chinchillada
 {
     public class BidirectionTweenComponent : AutoRefBehaviour, IBidirectionalTweener
     {
-        [SerializeField] private IBidirectionalTweener tweener;
+        [SerializeReference] private IBidirectionalTweener tweener;
+        
         public void TweenForward() => this.tweener.TweenForward();
 
         public void TweenBackward() => this.tweener.TweenBackward();
@@ -14,5 +16,20 @@ namespace Chinchillada
         public void ForceForward() => this.tweener.ForceForward();
 
         public void ForceBackward() => this.tweener.ForceBackward();
+        
+        [Serializable]
+        public class Reference : IBidirectionalTweener
+        {
+            [SerializeField] private BidirectionTweenComponent component;
+            public void TweenForward() => this.component.TweenForward();
+
+            public void TweenBackward() => this.component.TweenBackward();
+
+            public void TweenBackward(TweenCallback onFinished) => this.component.TweenBackward(onFinished);
+
+            public void ForceForward() => this.component.ForceForward();
+
+            public void ForceBackward() => this.component.ForceBackward();
+        }
     }
 }
