05e08696e92807e031b3835ac6e90663a88a6334
Jordy <jordy_van_opstal@live.nl>
2024-04-24 13:10:18 +0200

    add weightedItem

---
+++ Runtime/Datastructures/WeightedItem.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+namespace Chinchillada
+{
+    [Serializable]
+    public class WeightedItem<T>
+    {
+        [SerializeField] private T item;
+
+        [SerializeField] private int weight;
+
+        public T   Item   => this.item;
+        public int Weight => this.weight;
+
+        public WeightedItem()
+        {
+        }
+
+        public WeightedItem(T item, int weight = 1)
+        {
+            this.item = item;
+            this.weight = weight;
+        }
+
+        public void Deconstruct(out T item, out int weight)
+        {
+            item = this.item;
+            weight = this.weight;
+        }
+    }
+}
