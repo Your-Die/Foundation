18fab0848e16d5a6406ad286d1aaebe852fe9248
Jordy <jordy_van_opstal@live.nl>
2024-04-24 13:10:13 +0200

    Bring back distribution interface

---
+++ Runtime/Interfaces/IDistribution.cs
@@ -0,0 +1,15 @@
+﻿namespace Chinchillada
+{
+    /// <summary>
+    /// Interface for distributions that can be sampled.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IDistribution<out T>
+    {
+        /// <summary>
+        /// Sample the distribution.
+        /// </summary>
+        /// <returns>The sample.</returns>
+        T Sample(IRNG random);
+    }
+}
