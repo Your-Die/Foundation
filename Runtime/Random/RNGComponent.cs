958257af848ca8e5fc1fcd957ef64e7e9b5e5bc9
Jordy van Opstal <jvanopstal@bhvr.com>
2024-04-26 16:30:43 +0200

    add RNGComponent.Reference

--- Runtime/Random/RNGComponent.cs
+++ Runtime/Random/RNGComponent.cs
@@ -1,8 +1,8 @@
-﻿namespace Chinchillada
-{
-    using Chinchillada;
-    using UnityEngine;
+﻿using System;
+using UnityEngine;
 
+namespace Chinchillada
+{
     public class RNGComponent : AutoRefBehaviour, IRNG
     {
         [SerializeField] private IRNG random;
@@ -16,5 +16,21 @@
         public int Range(int min, int max, bool inclusive = false) => this.random.Range(min, max, inclusive);
 
         public void Initialize() => this.random.Initialize();
+
+        [Serializable]
+        public class Reference : IRNG
+        {
+            [SerializeField] private RNGComponent component;
+
+            public void Initialize() => this.component.Initialize();
+
+            public void SetSeed(int seed) => this.component.SetSeed(seed);
+
+            public float Float() => this.component.Float();
+
+            public float Range(float min, float max) => this.component.Range(min, max);
+
+            public int Range(int min, int max, bool inclusive = false) => this.component.Range(min, max, inclusive);
+        }
     }
 }
